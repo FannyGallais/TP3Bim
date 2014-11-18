68d4610cee62fc645d6fcd04d588f8e37410e7bb
FannyGalais <gallais.fanny@gmail.com>
2014-11-18 18:58:35 +0100

    Changed Struct Image into class Image in Image.h

--- Image.h
+++ Image.h
@@ -1,14 +1,21 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-  typedef struct
-  {
-    int height;
-    int width;
-    unsigned char * pix;
-  }image;
-
+class Image
+{
+ public :
   void ppm_write_to_file(image * im, const char* filename);
   void ppm_read_from_file(image * im, const char* filename);
   void ppm_desaturate(image * im);
   void ppm_shrink(image * im, int factor);
+
+ private :
+  int height;
+  int width;
+  u_char * pix;
+
+};
 
 
